294718344599df630c8055ea2389087df78765a2
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-05-26 00:21:45 -0300

    agrego ejercicio 1

---
+++ js/ejercicio1.js
@@ -0,0 +1,20 @@
+let meses = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+
+document.write("Meses del año:<br>");
+
+for (let i = 0; i < meses.length; i++) {
+  document.write(meses[i] + "<br>");
+}
