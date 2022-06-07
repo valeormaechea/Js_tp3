7967948b9c0bb5a01da6e40d3de0c5e9101d04fe
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-06-07 09:57:38 -0300

    agrego ejercicio 3

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,52 +1,39 @@
 let arreglo = [];
+let arregloCant = [];
+let max = 0;
+let recorrido = 0;
 
+//Voy cargando la suma de los dados en un arreglo
 for (i = 0; i < 50; i++) {
   let dado1 = Math.floor(Math.random() * 6) + 1;
   let dado2 = Math.floor(Math.random() * 6) + 1;
 
-  console.log(dado1);
-  console.log(dado2);
-
-  suma = dado1 + dado2;
-
-  arreglo[i] = suma;
-  contar(suma);
+  arreglo[i] = dado1 + dado2;
+  console.log(arreglo[i]);
 }
 
-function contar(suma) {
-  switch (suma) {
-    case 2:
-      let contador2 = contador2 + 1;
-      break;
-    case 3:
-      let contador3 = contador3 + 1;
-      break;
-    case 4:
-      let contador4 = contador4 + 1;
-      break;
-    case 5:
-      let contador5 = contador5 + 1;
-      break;
-    case 6:
-      let contador6 = contador6 + 1;
-      break;
-    case 7:
-      let contador7 = contador7 + 1;
-      break;
-    case 8:
-      let contador8 = contador8 + 1;
-      break;
-    case 9:
-      let contador9 = contador9 + 1;
-      break;
-    case 10:
-      let contador10 = contador10 + 1;
-      break;
-    case 11:
-      let contador11 = contador11 + 1;
-      break;
-    case 12:
-      let contador12 = contador12 + 1;
-      break;
+//Sumo la cantidad de apariciones de cada suma, y las cargo en otro arreglo (arregloCant)
+for (i = 2; i <= 12; i++) {
+  let cantAparic = 0;
+  for (j = 0; j < 50; j++) {
+    if (arreglo[j] == i) {
+      cantAparic = cantAparic + 1;
+    }
+  }
+
+  document.write(`La suma ${i} aparece ${cantAparic} veces.<br>`);
+  arregloCant[recorrido] = cantAparic;
+  recorrido++;
+}
+
+//Busco el mayor de los elementos en el arreglo de cantidades
+for (i = 0; i < 12; i++) {
+  if (arregloCant[i] > max) {
+    posicMax = i;
+    max = arregloCant[i];
+    console.log(posicMax);
   }
 }
+
+//Interpreto el mayor por suma en las posiciones, comparado con el arregloCant y la posicion que fueron tomando al sumar las apariciones
+document.write(`<br>La suma que sale mas veces es la del ${posicMax + 2}`);
